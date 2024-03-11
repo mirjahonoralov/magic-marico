273acd6aaf77fda62be616693dc61c7b86c53641
mirjahon <mirjahonoralov@gmail.com>
2024-03-11 18:25:10 +0500

    responsive extra

--- src/pages/Pricing/components/Extra.tsx
+++ src/pages/Pricing/components/Extra.tsx
@@ -11,24 +11,30 @@ const Extra = () => {
   return (
     <Stack gap="80px" mt="80px">
       <Stack gap="30px">
-        <Text fw="600" fs="75px" textAlign="center">
+        <Text fw="600" fontSize={{ lg: "75px", md: "60px", xs: "40px" }} textAlign="center">
           Experts Agree
         </Text>
         <img src={img} alt="" />
       </Stack>
 
-      <FlexWrapper sx={{ backgroundImage: `url(${audience})` }} height="800px">
-        <Text fw="600" fs="70px" textAlign="center">
+      <FlexWrapper
+        sx={{ backgroundImage: `url(${audience})` }}
+        height={{ md: "800px", xs: "500px" }}
+        margin={{ md: "0 -30px", xs: "0 -20px" }}
+      >
+        <Text fw="600" fontSize={{ lg: "70px", md: "60px", xs: "30px" }} textAlign="center" p="0 20px">
           We've helped over 1,000 creatorsre claim control of their audience.
         </Text>
       </FlexWrapper>
 
       <Stack gap="20px" alignItems="center">
-        <Box component="img" width="164px" src={logo} alt="" />
-        <Text fw="700" fs="100px" lineHeight="100%">
+        <Box component="img" width={{ md: "164px", xs: "100px" }} src={logo} alt="" />
+        <Text fw="700" fontSize={{ lg: "100px", md: "80px", xs: "40px" }} lineHeight="100%" textAlign="center">
           Get Started Now
         </Text>
-        <Text fs="43px">Setup is easy and takes under 5 minutes.</Text>
+        <Text fontSize={{ lg: "43px", md: "25px", xs: "20px" }} textAlign="center">
+          Setup is easy and takes under 5 minutes.
+        </Text>
         <CustomButton>Get Started Now</CustomButton>
         <FlexWrapper gap="10px">
           <Box component="img" src={dot} alt="" />
